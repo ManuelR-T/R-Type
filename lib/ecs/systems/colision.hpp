46f1c30f0eebe5b4efe7213f96ed9c9a2c5a079b
Manuel Tomé <manuel.tome@epitech.eu>
2024-09-17 00:55:32 +0200

    add: basic ecs

---
+++ lib/ecs/systems/colision.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** R-type
+** File description:
+** colision
+*/
+
+#pragma once
+
+#include "../core/registry.hpp"
+
+namespace systems {
+
+
+void collision(registry &reg);
+
+} // namespace systems
---
+++ lib/ecs/systems/draw.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** R-type
+** File description:
+** draw
+*/
+
+#pragma once
+
+#include "../core/registry.hpp"
+#include <SFML/Graphics.hpp>
+
+namespace systems {
+
+void draw(registry &reg, sf::RenderWindow &window);
+
+} // namespace systems
