1340e839f202c17970f873154969b07c523b8f28
rahul.chander <rahul.chander@epitech.eu>
2024-10-02 23:29:48 +0200

    add: Protocol done and gitignore too

--- include/RTypeConst.hpp
+++ include/RTypeConst.hpp
@@ -11,6 +11,8 @@ namespace rt {
 constexpr unsigned int FPS_LIMIT = 60;
 
 constexpr float MOVEMENT_TICK_RATE = 30.0;
+constexpr float AI_ACTING_TICK_RATE = 10.0;
+constexpr float SEND_PACKETS_TICK_RATE = 10.0;
 
 constexpr unsigned int SCREEN_WIDTH = 1920 / 1.6;
 constexpr unsigned int SCREEN_HEIGHT = 1080 / 1.6;
